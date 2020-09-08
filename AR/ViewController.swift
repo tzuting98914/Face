6ae9189221647193b7049fdf90ba29542a18cafb
tzuting98914 <bee87914@gmail.com>
2020-09-08 21:12:58 +0800

    Initial Commit

---
+++ AR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR
+//
+//  Created by MIS@NSYSU on 2020/9/8.
+//  Copyright © 2020年 knowhom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
